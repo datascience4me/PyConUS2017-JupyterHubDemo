124824b47cf1bcd60280eb3122bf252d0b274ae0
mjbright <github.com@mjbright.net>
2017-05-18 16:31:47 +0200

    Added info about -a option

--- scripts/purge.sh
+++ scripts/purge.sh
@@ -1,19 +1,26 @@
 
 . .setup
 
+TIME=time
+
+echo
+echo "Use option -a to delete also the gcloud cluster"
+echo
+
+
 if [ "$1" = "-a" ];then
-    CMD="gcloud container clusters delete --zone=$ZONE $NAMESPACE"
+    CMD="$TIME gcloud container clusters delete --zone=$ZONE $NAMESPACE"
     echo $CMD
     eval $CMD
     exit 0
 fi
 
-CMD="helm delete --purge $RELEASE"
+CMD="$TIME helm delete --purge $RELEASE"
 echo $CMD
 eval $CMD
 
 
-CMD="kubectl --namespace=$NAMESPACE delete pods --all"
+CMD="$TIME kubectl --namespace=$NAMESPACE delete pods --all"
 echo $CMD
 eval $CMD
 
